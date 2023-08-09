ddb97c0b562ebdf30f14fb127bf4dbb2d4565cd2
tiagoferreira18 <tiagoferreira18@gmail.com>
2023-08-09 10:42:58 -0300

    Componentes, css e JSX

---
+++ organo/src/componentes/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+function Banner(){
+    //JSX
+    return(
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner principal da página organo" />
+        </header>
+    )
+}
+export default Banner
